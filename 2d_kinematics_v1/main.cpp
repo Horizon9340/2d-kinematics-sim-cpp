aa289c7384c9a6e6889bd1c054fef72a68039764
Joshua Hancock <jaaronhancock10@gmail.com>
2025-12-30 10:48:09 -0600

    Fixed velocity components bug

--- 2d_kinematics_v1/main.cpp
+++ 2d_kinematics_v1/main.cpp
@@ -11,21 +11,23 @@ double y = 0;
 double v = 10;
 double theta = 30;
 
+double vx = v * cos(theta * 3.14159265358979323846 / 180);
+double vy = v * sin(theta * 3.14159265358979323846 / 180);
+
 double updateX(double x, double velocityX) {
   x = x + velocityX * dt;
+  vx = velocityX;
   return x;
 }
 
 double updateY(double y, double velocityY) {
   velocityY = velocityY - gravity * dt;
+  vy = velocityY;
   y = y + velocityY * dt;
   return y;
 }
 
 int main () {
-  double vx = v * cos(theta * 3.14159265358979323846 / 180);
-  double vy = v * sin(theta * 3.14159265358979323846 / 180);
-
   while (y >= 0) {
     x = updateX(x, vx);
     y = updateY(y, vy);
