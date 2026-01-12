6e991b7450bcf9157defb70a98d7f5afff964e82
Joshua Hancock <jaaronhancock10@gmail.com>
2026-01-12 00:42:48 -0600

    Created learn-opengl folder

---
+++ learn-opengl/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+
+int main() {
+  // Initialize GLFW window
+  if(!glfwInit()){
+    std::cerr << "Failed to initialize GLFW\n";
+    return 1;
+  }
+
+  // ask for OpenGL context
+  // macOS supports up to 4.1, core profile
+
+
+
+  return 0;
+}
+
+
+// VERY MUCH UNFINISHED, JUST DON'T LOOK HERE FOR NOW
